755a47d29e5b9ac05d8aad09ddafd80b602f3f28
zeroCalm <smith.timothyh@gmail.com>
2017-02-01 12:16:39 -0800

    function shows which key is pressed

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-(document).ready(function(){
 
+document.addEventListener("keydown", function(event) {
+  var key=event.which;
 
-  
+  console.log(key);
+})
 
-});
+function whichKey(key){
+  if(key === 65){
+    console.log("You Pressed A");
+  }else{
+    console.log("You Pressed L");
+  }
+whichKey();
+
+}
+
+//
+// $(document).ready(function(){
+//
+//
+//
+//
+// });
