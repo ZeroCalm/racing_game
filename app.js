63ff9a1201b59b18148a3564df4b804531d6eb9a
zeroCalm <smith.timothyh@gmail.com>
2017-02-01 14:21:11 -0800

    attempt at clearing div on keypress

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 
 document.addEventListener("keydown", function(event) {
   var key=event.which;
-
-  console.log(key);
-})
-
-function whichKey(key){
   if(key === 65){
-    console.log("You Pressed A");
+    $(this).innerHTML = "";
   }else{
     console.log("You Pressed L");
   }
-whichKey();
 
-}
 
-//
-// $(document).ready(function(){
-//
-//
-//
-//
-// });
+    });
+
+
+    //
+    // $("button").click(function() {
+
+
+
+
+$(document).ready(function(){
+
+
+
+
+
+});
