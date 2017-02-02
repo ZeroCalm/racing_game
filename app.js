f42df9d25b1b7a32d8b3603ce254fb1329759b3a
zeroCalm <smith.timothyh@gmail.com>
2017-02-02 08:40:11 -0800

    Cars don't move, but buttonpush is awesome, A and L keys

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 document.addEventListener("keydown", function(event) {
   var key=event.which;
   if(key === 65){
-    $(this).innerHTML = "";
+    console.log("You Pressed L");
   }else{
     console.log("You Pressed L");
   }
