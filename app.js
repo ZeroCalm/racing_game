18acbfc334bf63db12c053643d4ede3bc863d34c
zeroCalm <smith.timothyh@gmail.com>
2017-02-02 08:50:02 -0800

    fixed mistype in appjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 document.addEventListener("keydown", function(event) {
   var key=event.which;
   if(key === 65){
-    console.log("You Pressed L");
+    console.log("You Pressed A");
   }else{
     console.log("You Pressed L");
   }
